d6b4ba814713dc30907d2af5fbb0e444d37d598b
kiko <feras221vladimir@gmail.com>
2026-01-17 20:55:43 +0300

    chore(fmt): format contracts with forge fmt

--- src/MarketPlace.sol
+++ src/MarketPlace.sol
@@ -8,7 +8,7 @@ interface IOrderRegistry {
 contract MarketPlace {
     /* ========== STORAGE ========== */
 
-    struct ListingItem{
+    struct ListingItem {
         string itemName;
         uint256 itemId;
         address seller;
@@ -18,15 +18,15 @@ contract MarketPlace {
     address public admin;
     IOrderRegistry public orderRegistry;
     mapping(uint256 => ListingItem) public items;
-    mapping (address => bool) isVip;
-    mapping(address =>bool) isSeller;
+    mapping(address => bool) isVip;
+    mapping(address => bool) isSeller;
     uint256 nextItemListingId = 1;
     /* ========== EVENTS ========== */
     event Withdraw(address indexed who, uint256 amount);
     event CreateListingItem(uint256 indexed itemId, address indexed who, uint128 quantity, uint256 price);
     event PriceUpdated(uint256 indexed itemId, uint256 oldPrice, uint256 newPrice);
     event QuantityUpdated(uint256 indexed itemId, uint256 oldQty, uint256 newQty);
-    event VipSet(address indexed vip ,bool isVip);
+    event VipSet(address indexed vip, bool isVip);
     event SellerSet(address indexed seller, bool isSeller);
     /* ========== MODIFIERS ========== */
     modifier onlyAdmin() {
@@ -34,8 +34,8 @@ contract MarketPlace {
         _;
     }
     modifier OnlyAdminOrSeller(uint256 _itemId) {
-        if(items[_itemId].seller == address(0)) revert ItemNotFound();
-        if(msg.sender != items[_itemId].seller && msg.sender != admin) revert NotSeller();
+        if (items[_itemId].seller == address(0)) revert ItemNotFound();
+        if (msg.sender != items[_itemId].seller && msg.sender != admin) revert NotSeller();
         _;
     }
     /* ========== ERRORS ========== */
@@ -55,51 +55,50 @@ contract MarketPlace {
         admin = msg.sender;
         orderRegistry = IOrderRegistry(orderRegistryAddress);
     }
+
     /* ========== ADMIN ACTION ========== */
-    function setVip(address buyerVip,bool status) external onlyAdmin {
-        if(buyerVip == address(0)) revert ZeroAddress();
+    function setVip(address buyerVip, bool status) external onlyAdmin {
+        if (buyerVip == address(0)) revert ZeroAddress();
         isVip[buyerVip] = status;
 
         emit VipSet(buyerVip, status);
     }
+
     function setSeller(address seller, bool status) external onlyAdmin {
-        if(seller == address(0)) revert ZeroAddress();
+        if (seller == address(0)) revert ZeroAddress();
         isSeller[seller] = status;
 
         emit SellerSet(seller, status);
     }
+
     /* ========== SELLER ACTION ========== */
-    function createItem(string memory _name , uint128 _quantity, uint256 _price) public {
-        if(_price == 0) revert PriceCantBeZero();
-        if(_quantity == 0) revert QuantityCantBeZero();
+    function createItem(string memory _name, uint128 _quantity, uint256 _price) public {
+        if (_price == 0) revert PriceCantBeZero();
+        if (_quantity == 0) revert QuantityCantBeZero();
         uint256 listingId = nextItemListingId++;
         items[listingId] = ListingItem({
-            itemName : _name ,
-            itemId : listingId,
-            seller : msg.sender,
-            quantity : _quantity,
-            priceWei : _price
+            itemName: _name, itemId: listingId, seller: msg.sender, quantity: _quantity, priceWei: _price
         });
 
-        emit CreateListingItem(listingId, msg.sender,_quantity, _price);
+        emit CreateListingItem(listingId, msg.sender, _quantity, _price);
     }
 
-    function setItemPrice(uint256 _itemId, uint256 _newPrice) external OnlyAdminOrSeller(_itemId){
+    function setItemPrice(uint256 _itemId, uint256 _newPrice) external OnlyAdminOrSeller(_itemId) {
         ListingItem storage it = items[_itemId];
-        if(_newPrice == 0) revert PriceCantBeZero();
+        if (_newPrice == 0) revert PriceCantBeZero();
         uint256 oldPrice = it.priceWei;
         it.priceWei = _newPrice;
 
         emit PriceUpdated(_itemId, oldPrice, _newPrice);
     }
-    function setQuantity(uint256 _itemId, uint128 _newQuantity) external OnlyAdminOrSeller(_itemId){
+
+    function setQuantity(uint256 _itemId, uint128 _newQuantity) external OnlyAdminOrSeller(_itemId) {
         ListingItem storage it = items[_itemId];
-        if(_newQuantity == 0) revert QuantityCantBeZero();
+        if (_newQuantity == 0) revert QuantityCantBeZero();
         uint256 oldQuantity = it.quantity;
-        it.quantity = _newQuantity; 
+        it.quantity = _newQuantity;
 
         emit QuantityUpdated(_itemId, oldQuantity, _newQuantity);
-
     }
     /* ========== USER ACTION ========== */
     // function buy(uint256 _itemId, uint128 _amount) external returns (uint256 orderId) {
@@ -107,6 +106,5 @@ contract MarketPlace {
     //     if(it.seller == address(0)) revert ItemNotFound();
     //     if(_amount == 0) revert AmountCantBeZero();
 
-        
     // }
 }
